afa41f3b15b4d04104fec02201a929c1ea6443d1
Christos Strouthos <mrdestro29@gmail.com>
2016-01-22 15:55:41 +0200

    This is v1

---
+++ Script.js
@@ -0,0 +1,27 @@
+
+function refuel() {
+    alert("Coming soon Boyz!!!");
+}
+
+function carwash() {
+    alert("The car has been washed");
+}
+
+function oil_check() {
+    alert("The oil has been checked");
+}
+
+function tyres_check() {
+    alert("The Tyres has been checked");
+}
+
+function carwash1() {
+    var txt;
+    var r = confirm("Are you sure?");
+    if (r == true) {
+        alert("The car has been washed");
+    } else {
+
+    }
+
+}
