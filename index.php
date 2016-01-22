e6125d909d16297a08942cfd3c5cc895cd040d17
Christos Strouthos <mrdestro29@gmail.com>
2016-01-22 15:54:29 +0200

    This is v1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" type="text/css" href="StyleSheet.css"/>
+    <script type="text/javascript" src="JavaScript.js"></script>
+    <title></title>
+</head>
+<body>
+    <div id="main">
+      <div class="block">
+            <div class="centered">
+                <div><input type="button" class="button" value="Refuel" onclick="refuel()" /></div>
+                <div><input type="button" class="button" value="Carwash" onclick="carwash1()"/></div>
+                <div><input type="button" class="button" value="Oil check" onclick="oil_check()"/></div>
+                <div><input type="button" class="button" value="Tyres Check" onclick="tyres_check()"/></div>
+  </div>
+
+
+</div>
+
+         </div>
+</body>
+</html>
