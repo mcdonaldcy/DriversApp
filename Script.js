c99cfeca9730fa84de4c03fa68e0f797a52fe9a6
Christos Strouthos <mrdestro29@gmail.com>
2016-02-01 08:23:37 +0200

    New version

--- Script.js
+++ Script.js
@@ -1,8 +1,4 @@
 
-function refuel() {
-    alert("Coming soon Boyz!!!");
-}
-
 function carwash() {
     alert("The car has been washed");
 }
@@ -21,33 +17,5 @@ function carwash1() {
     if (r == true) {
         alert("The car has been washed");
     } else {
-
-    }
-
+  }
 }
-
-
-$('#box').click(function buttonAction() {
-  $("#dialog-confirm").html("Do you want to do the thing?");
-
-  // Define the Dialog and its properties.
-  $("#dialog-confirm").dialog({
-    resizable: false,
-    modal: true,
-    title: "Do the thing?",
-    height: 250,
-    width: 400,
-    buttons: {
-      "Yes": function() {
-        $(this).dialog('close');
-        alert("Yes, do the thing");
-      },
-      "No": function() {
-        $(this).dialog('close');
-        alert("Nope, don't do the thing");
-      }
-    }
-  });
-});
-
-$('#box').click(buttonAction);
